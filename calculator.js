3d0bba72039fd82a619bbdc0642a919e20636d39
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-06-14 01:58:26 +0100

    update

---
+++ calculator.js
@@ -0,0 +1,10 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
