887465cbe794f4cbde6ad81f0ea26d4ad10abf1e
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-06-14 01:11:57 +0100

    update

--- quiz.js
+++ quiz.js
@@ -1,4 +1,11 @@
 function checkAnswer() {
+    let correctAnswer = "4";
 
+    let userAnswer = document.querySelector("quiz").value;
+    if (userAnswer === correctAnswer) {
+        document.getElementById("feedback").innerHTML = "Correct! Well done!";
+    } else {
+        document.getElementById("feedback").innerHTML = "That's incorrect. Try again!";
+    }
 
 }
